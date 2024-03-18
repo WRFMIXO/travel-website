9499d76a3777c36f14db687d3a77221dfa8c43a3
WRFMIXO <jakim2621@gmail.com>
2024-03-18 20:28:09 +0100

    mise à jour des routes

--- src/Routes.js
+++ src/Routes.js
@@ -12,12 +12,11 @@ const ApplicationRoutes = () =>  {
         <BrowserRouter>
             <Routes>
                 <Route path="/travel-website/" element={ <HomePage /> } />
-                <Route path="/travel-website/services/" element={ <ServicesPage /> } />
-                <Route path="/travel-website/team/" element={ <TeamPage /> } />
-                <Route path="/travel-website/contacts/" element={ <ContactPage /> } />
-                <Route path="/travel-website/processes/" element={ <ProcessesPage /> } />
-                <Route path="/travel-website/faq/" element={ <FAQ /> } />
-
+                <Route path="/travel-website/services" element={ <ServicesPage /> } />
+                <Route path="/travel-website/team" element={ <TeamPage /> } />
+                <Route path="/travel-website/contacts" element={ <ContactPage /> } />
+                <Route path="/travel-website/processes" element={ <ProcessesPage /> } />
+                <Route path="/travel-website/faq" element={ <FAQ /> } />
             </Routes>
         </BrowserRouter>
     );
